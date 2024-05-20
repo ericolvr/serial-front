b5a1dea8f509cadd7d34a07fad5651b0f48943a1
Erico Oliveira <ericolvr@gmail.com>
2024-05-20 06:57:39 -0300

    edit function

---
+++ src/screens/client/edit.tsx
@@ -0,0 +1,176 @@
+import { useContext, useState, useEffect } from "react";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form"
+import { Toaster, toast } from 'sonner'
+import { Button } from "@/components/ui/button";
+import { z } from "zod"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Link, useNavigate, useParams } from 'react-router-dom'; 
+
+import { AuthContext } from "@/contexts/general";
+import { Sidebar } from "@/components/app/sidebar";
+import { UserActions } from "@/components/app/userActions";
+import { Messages } from "@/components/app/messages";
+import { Input } from "@/components/ui/input";
+import ApiClient from "./service"; 
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
+const FormSchema = z.object({
+    name: z.string().nonempty({ message: 'Informe o nome do cliente' }),
+});
+
+
+export function ClientEdit() {
+    const { id } = useParams<{ id: string }>();
+    const { opened } = useContext(AuthContext);
+    const navigate = useNavigate(); 
+    const [client, setClient] = useState<any>({});
+    const [selectedClient, setSelectedClient] = useState('');
+    const [loading, setLoading] = useState(true);
+
+
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            name: '', 
+        },
+    })
+
+    async function onSubmit(data: z.infer<typeof FormSchema>) {
+        console.log(data, '----on submit')
+        try {
+            const response = await ApiClient.Update({ id, data });
+            if (response == 200) {
+                navigate('/client')
+            } else {
+                toast.error('Erro ao editar o cliente');
+            }
+
+        } catch (error) {
+            console.log(error, 'error')
+        }
+    }
+
+    const getClient = async () => {
+        const response = await ApiClient.GetClient({id});
+        if (response) {
+            setSelectedClient(response);
+            setClient(response)
+            setLoading(false)
+            if(response.name) {
+                form.setValue('name', response.name.toString());
+            } else {
+                console.log('VAZIO')
+            }
+        }
+    }
+
+    useEffect(() => {
+        getClient();
+        
+    }, []);
+
+
+    return (
+        <div className="flex">
+            <Toaster position="top-right" />
+            <Sidebar />
+
+            <div className={ opened ? "flex-1 bg-[#f0f0f0]" : "flex-1 bg-[#f0f0f0] h-dvh" }>
+                <section className="flex pt-6 pr-10 pb-6 pl-9 justify-between">
+                    <div className="justify-start pt-3 pl-3">
+                        <Breadcrumb>
+                            <BreadcrumbList>
+                                <BreadcrumbItem>
+                                    <Link to="/">
+                                        <BreadcrumbLink className="font-mono text-[#000000] text-md">Dashboard</BreadcrumbLink>
+                                    </Link>
+                                </BreadcrumbItem>
+                                <BreadcrumbSeparator />
+                                <BreadcrumbItem>
+                                <Link to="/client">
+                                    <BreadcrumbLink className="font-mono text-[#000000] text-md">Clientes</BreadcrumbLink>
+                                </Link>
+                                </BreadcrumbItem>
+                                <BreadcrumbSeparator />
+                                <BreadcrumbItem>
+                                    <BreadcrumbPage className="font-mono text-gray-500 text-md">Adicionar</BreadcrumbPage>
+                                </BreadcrumbItem>
+                            </BreadcrumbList>
+                        </Breadcrumb>
+                    </div>
+                    <div className='justify-end'>
+                        <div className="flex justify-between">
+                            <Messages />
+                            <UserActions />
+                        </div>
+                    </div>
+                </section>
+
+                <section className=" h-auto pt-2 pl-10 pr-10">
+                    <div className="flex flex-row justify-between mt-1">
+                        <div className="bg-white shadow-md p-10 w-full rounded-md">
+                            <Form {...form}>
+                                <form onSubmit={form.handleSubmit(onSubmit)}>
+                                    <div className="flex items-center">
+                                        <div className="w-full mr-8">
+                                        <FormField
+                                            control={form.control}
+                                            name="name" 
+                                            render={({field}) => (
+                                                <FormItem>
+                                                    <FormLabel>Fase</FormLabel>
+                                                    { loading ? (
+                                                        <p>loading</p>
+                                                    ) : (
+                                                        <Select
+                                                            defaultValue={client && client.name ? client.name.toString() : ''}
+                                                            onValueChange={value => {
+                                                                field.onChange(value);
+                                                            }}
+                                                        >
+                                                            <FormControl>
+                                                                <SelectTrigger>
+                                                                    <SelectValue placeholder="Selecione o Cliente" />
+                                                                </SelectTrigger>
+                                                            </FormControl>
+                                                            <SelectContent>
+                                                                <SelectItem key={1} value={client.name}>{client.name}</SelectItem>
+                                                                <SelectItem key={2} value="IBM">IBM</SelectItem>
+                                                                <SelectItem key={3} value="Oracle">Oracle</SelectItem>
+                                                            </SelectContent>
+                                                        </Select>
+                                                    )}
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                        </div>
+                                    </div>
+                                    <div className="pt-6">
+                                        <Button type="submit" className="bg-black font-mono">Salvar</Button>
+                                    </div>
+                                </form>    
+                            </Form>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    )
+}
