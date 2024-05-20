d01209dc249460f494d7bcab6fa33d5a4e3473d9
Erico Oliveira <ericolvr@gmail.com>
2024-05-20 19:10:48 -0300

    remove delete

--- src/screens/register/columns.tsx
+++ src/screens/register/columns.tsx
@@ -95,9 +95,6 @@ export const columns: ColumnDef<register>[] = [
                     <Link to={`/register/edit/${line.id}`} className="text-right ml-8">
                         <Pencil strokeWidth={1.75} className="h-5 w-5 text-black hover:text-gray-800" />
                     </Link>
-                    <Link to={`/register/delete/${line.id}`} className="ml-8">
-                        <Trash2 strokeWidth={1.75} className="h-5 w-5 text-red-600 hover:text-red-800" />
-                    </Link>
                 </div>
                 
             )
