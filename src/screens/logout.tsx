2b9eb3cf3f6bd1c367cb14bc2219cb178cb976b0
Erico Oliveira <ericolvr@gmail.com>
2024-05-20 19:10:08 -0300

    remove import

--- src/screens/logout.tsx
+++ src/screens/logout.tsx
@@ -3,30 +3,25 @@ import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '@/contexts/general'
 
 export function Logout() {
-    console.log('------------------------------------ logout')
+    
     const { HandleAuthenticated }  = useContext(AuthContext);
     const navigate = useNavigate();
-    const [hasNavigated, setHasNavigated] = useState(false);
-
+    
     const deleteUserData = () => {  
-        HandleAuthenticated(false)
         localStorage.removeItem('name')
         localStorage.removeItem('token')
         localStorage.removeItem('role')
+        HandleAuthenticated(false)
     }
 
     useEffect(() => {
         deleteUserData();
-        // if (!hasNavigated) {
-        //     navigate('/');
-        //     setHasNavigated(true);
-        // }
         setTimeout(() => {
             navigate('/');
-        }, 1000);
+        }, 500);
     }, [])
 
     return (
-        <h1>Logout</h1>
+        null
     );
 }
