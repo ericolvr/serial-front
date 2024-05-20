0a13bb5b66872b9ac2776ba4107045feb47e97b8
Erico Oliveira <ericolvr@gmail.com>
2024-05-20 06:57:09 -0300

    private routes

---
+++ src/routes/privateRoutes.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useContext } from 'react';
+import { Navigate } from 'react-router-dom'
+import { AuthContext } from '@/contexts/general';
+import Storage from '../storage';
+
+
+export function PrivateRoutes({ children }) {
+    const { authenticated, HandleAuthenticated } = useContext(AuthContext);
+
+    useEffect(() => {
+        const checkToken = async () => {
+            const token = await Storage.RetriveUserToken('token');
+            if (token || token != null){
+                HandleAuthenticated(true);
+            } else {
+                HandleAuthenticated(false)
+            }
+        };
+
+        checkToken();
+    }, []);
+
+    return authenticated ? children : <Navigate to='/' />
+}
+
+
+
