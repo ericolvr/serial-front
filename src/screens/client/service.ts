634465148bf27ad953ef82bc646699a994c944c3
Erico Oliveira <ericolvr@gmail.com>
2024-05-20 06:57:47 -0300

    update

--- src/screens/client/service.ts
+++ src/screens/client/service.ts
@@ -25,6 +25,30 @@ class ApiClient {
             console.log(error);
         }
     }
+
+    static async GetClient({ id }: { id: string }) {
+        try {
+            const response = await axios.get(`${BASE_URL}/clients/${id}`);
+            if (response.status === 200) {
+                return response.data;
+            }
+            return response.data;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async Update({ id, data }) {
+        console.log(id, data)
+        try{ 
+            const response = await axios.patch(`${BASE_URL}/clients/update/${id}`, data)
+            if (response.status == 200) {
+                return response.status
+            }
+        } catch (error) {
+            console.log(error)
+        } 
+    }
 }
 
 export default ApiClient;
