2bb40ff73adf3630b0f127d933090b93c7629427
Erico Oliveira <ericolvr@gmail.com>
2024-05-21 18:56:46 -0300

    get iuser

--- src/screens/user/service.ts
+++ src/screens/user/service.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const BASE_URL = "http://192.168.2.26:8888"
-// process.env.REACT_APP_BASE_URL;
+// const BASE_URL = "http://192.168.2.26:8888"
+const BASE_URL = import .meta.env.VITE_API_URL;
 
 
 interface Data {
@@ -33,6 +33,29 @@ class ApiUser {
         } catch (error) {
             console.log(error);
         }
+    }
+
+    static async GetUser({ id }: { id: string }) {
+        try {
+            const response = await axios.get(`${BASE_URL}/users/${id}`);
+            if (response.status === 200) {
+                return response.data;
+            }
+            return response.data;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async Update({ id, data }) {
+        try{ 
+            const response = await axios.patch(`${BASE_URL}/registers/update/${id}`, data)
+            if (response.status == 200) {
+                return response.status
+            }
+        } catch (error) {
+            console.log(error)
+        } 
     }
 }
 
