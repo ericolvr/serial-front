6fc08c1752da53f53e6369b2d448e9591b461f0c
Erico Oliveira <ericolvr@gmail.com>
2024-05-23 08:05:54 -0300

    response when already exist

--- src/screens/client/service.ts
+++ src/screens/client/service.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
-const BASE_URL = import .meta.env.VITE_API_URL;
-
+const BASE_URL ='http://192.168.0.25:8888'
+// = import .meta.env.VITE_API_URL;
+console.log(BASE_URL, 'BASE_URL')
 
 class ApiClient {
     static async List() {
