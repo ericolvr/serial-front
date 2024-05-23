7c36026757122e928a813d0c4f41c13c09f86e7a
Erico Oliveira <ericolvr@gmail.com>
2024-05-23 01:06:50 -0300

    loading

--- src/routes/privateRoutes.jsx
+++ src/routes/privateRoutes.jsx
@@ -1,27 +1,25 @@
-import { useEffect, useContext } from 'react';
+import { useEffect, useContext, useState } from 'react';
 import { Navigate } from 'react-router-dom'
 import { AuthContext } from '@/contexts/general';
 import Storage from '../storage';
 
-
 export function PrivateRoutes({ children }) {
     const { authenticated, HandleAuthenticated } = useContext(AuthContext);
+    const [loading, setLoading] = useState(true);
 
-    const checkToken = async () => {
-        const token = await Storage.RetriveUserToken('token');
-        if (token && token != null){
+    useEffect(() => {
+        const token = Storage.RetriveUserToken('token');
+        if (token != null){
             HandleAuthenticated(true);
         } else {
             HandleAuthenticated(false)
         }
-    };
-    
-    useEffect(() => {
-        checkToken();
+        setLoading(false);
     }, []);
+
+    if (loading) {
+        return <div>Loading...</div>; 
+    }
 
     return authenticated ? children : <Navigate to='/' />
 }
-
-
-
