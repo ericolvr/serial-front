8be364e3f73dd326e2fffbbb3692528a82dda6d3
Erico Oliveira <ericolvr@gmail.com>
2024-05-23 01:08:03 -0300

    mocked ip

--- src/screens/signin/service.ts
+++ src/screens/signin/service.ts
@@ -1,13 +1,14 @@
 import axios from 'axios';
 // import Utils from '../commons/utils';
-const BASE_URL = import .meta.env.VITE_API_URL;
-
+// const BASE_URL = import .meta.env.VITE_API_URL;
+const BASE_URL = 'http://192.168.0.25:8888';
 
 class ApiSignIn {
     static async GetToken({ data }) {
         try {
             const response = await axios.post(`${BASE_URL}/signin/token`, data);
             if (response.status === 201) {            
+                console.log(response.data);
                 const data = response.data;
                 // Utils.StoreUserData({ data });
                 return response.data;
