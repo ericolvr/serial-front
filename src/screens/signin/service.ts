f0a40c26da7ec23d48300b094da63e25b0d3191b
Erico Oliveira <ericolvr@gmail.com>
2024-05-23 09:46:09 -0300

    ip

--- src/screens/signin/service.ts
+++ src/screens/signin/service.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// const BASE_URL = import .meta.env.VITE_API_URL;
-const BASE_URL = 'http://192.168.0.25:8888';
+const BASE_URL = import .meta.env.VITE_API_URL;
+
 
 
 class ApiSignIn {
