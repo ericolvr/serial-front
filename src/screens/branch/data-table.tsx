92dc05bd670e7447c0d0c4ac57970c0072f1fdb7
Erico Oliveira <ericolvr@gmail.com>
2024-07-07 18:00:33 -0300

    remove dropdown import

--- src/screens/branch/data-table.tsx
+++ src/screens/branch/data-table.tsx
@@ -20,17 +20,11 @@ import {
 	TableHeader,
 	TableRow,
 } from "@/components/ui/table"
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuGroup,
-	DropdownMenuItem,
-	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { ChevronLeft, ChevronRight, Download, Printer, AlignRight } from "lucide-react"
-
+import { ChevronLeft, ChevronRight, Plus } from "lucide-react"
+import { Link } from "react-router-dom"
 
 interface DataTableProps<TData, TValue> {
 	columns: ColumnDef<TData, TValue>[]
@@ -67,33 +61,20 @@ export function DataTable<TData, TValue>({
 			<div className="">
 				<div className="flex justify-between items-center pb-4">
 					<Input
-						placeholder="Pesquisar por Uniorg ..."
+						placeholder="Pesquisar por Nome ..."
 						value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
 						onChange={(event) =>
 							table.getColumn("name")?.setFilterValue(event.target.value)
 						}
 						className="max-w-sm ml-2 mt-1 mb-2"
 					/>
-					
+
 					<div className="flex items-center space-x-6">
-						<DropdownMenu open={open} onOpenChange={setOpen}>
-							<DropdownMenuTrigger asChild>
-								<AlignRight strokeWidth={1.5} className="text-[#1A1C1E] w-8 h-8 hover:text-black cursor-pointer" />
-							</DropdownMenuTrigger>
-							<DropdownMenuContent align="end" className="w-[220px] p-3">
-								<DropdownMenuGroup>
-									<DropdownMenuItem className="p-3">
-										<Download strokeWidth={1.5} className="mr-2 h-5 w-5 text-black" />
-											Exportar 
-									</DropdownMenuItem>
-									<DropdownMenuItem className="p-3">
-										<Printer strokeWidth={1.5} className="mr-2 h-4 w-4" />
-										Imprimir 
-									</DropdownMenuItem>
-								</DropdownMenuGroup>
-							</DropdownMenuContent>
-						</DropdownMenu>
+						<Link to="/branch/add" className="flex justify-between items-center bg-black py-2.5 pl-2 pr-5 text-white rounded-lg border-2 border-black hover:bg-white hover:text-black hover:border-2 hover:border-black transition-colors duration-400">
+							<Plus strokeWidth={2.5} className=" h-4 w-4 tex-white mr-2" /> <span className="text-sm font-mono">Adicionar </span> 
+						</Link>
 					</div>
+					
       			</div>
 			<Table>
 			<TableHeader>
